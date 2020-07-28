609bc7a5d880412d793ee044a63f8e06123d401b
Nimish Shah <nimishshah2000@gmail.com>
2020-07-29 00:43:24 +0530

    seperated secured routes

--- routes/secured.routes.js
+++ routes/secured.routes.js
@@ -2,19 +2,15 @@ const router = require('express').Router();
 
 // Import Controllers Here
 const user = require('../controllers/users.controller');
-const login = require('../controllers/login.controller');
 
 //Index Route
 router.get('/',(req,res)=>{
     res.send('Index Route');
 });
 
-
 // User Routes
-router.post('/adduser',user.addUser);
-
-//Login
-router.post('/login',login.login);
+router.delete('/users',user.deleteUser);
+router.put('/users',user.updateUser);
 
 
 module.exports = router;
