1bf4ac40ce983f8185dbb5321f99426e1bc968b0
Nimish Shah <nimishshah2000@gmail.com>
2020-09-23 01:58:33 +0530

    add pagination

--- client/src/components/Website/components/Sections/shop/index.js
+++ client/src/components/Website/components/Sections/shop/index.js
@@ -3,7 +3,16 @@ import { Switch, Route, useRouteMatch } from 'react-router-dom';
 import Header from '../../../../utils/Header/Header.js';
 import HeaderLinks from './HeaderLinks';
 import { makeStyles } from '@material-ui/core/styles';
-import { Grid, Typography } from '@material-ui/core';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { Container, Grid, Paper } from '@material-ui/core';
+import Pagination from '@material-ui/lab/Pagination';
+
 import Loading from '../../../Loading.js';
 import Parallax from '../../../../utils/Parallax/Parallax.js';
 
@@ -16,12 +25,26 @@ const useStyle = makeStyles({
     position: 'relative',
     zIndex: '3',
   },
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+  paginationAdjust: {
+    alignContent: 'center',
+    margin: '25px 25px 25px 46%',
+  },
 });
 
 const ShopComp = (props) => {
   const classes = useStyle();
   const [data, setData] = React.useState(null);
+  const [page, setPage] = React.useState(1);
 
+  const handleChangePage = (e, val) => {
+    setPage(val);
+  };
   React.useEffect(() => {
     // Get data of the store from backend
     setData({
@@ -29,9 +52,106 @@ const ShopComp = (props) => {
       logo: 'url',
       maps_url:
         'https://www.google.com/maps/embed/v1/place?key=AIzaSyB387HYMPGb934bjW6nWNDt7jT6QhUmBTw&q=Space+Needle,Seattle+WA',
+      products: [
+        {
+          name: 'maggie',
+          price: '20',
+          image:
+            'https://www.ft.com/__origami/service/image/v2/images/raw/https%3A%2F%2Fs3-ap-northeast-1.amazonaws.com%2Fpsh-ex-ftnikkei-3937bb4%2Fimages%2F0%2F6%2F0%2F9%2F15379060-1-eng-GB%2FR20180823%20Maggi%20noodle_2048x1152.jpg?source=nar-cms',
+        },
+        {
+          name: 'maggie',
+          price: '20',
+          image:
+            'https://www.ft.com/__origami/service/image/v2/images/raw/https%3A%2F%2Fs3-ap-northeast-1.amazonaws.com%2Fpsh-ex-ftnikkei-3937bb4%2Fimages%2F0%2F6%2F0%2F9%2F15379060-1-eng-GB%2FR20180823%20Maggi%20noodle_2048x1152.jpg?source=nar-cms',
+        },
+        {
+          name: 'maggie',
+          price: '20',
+          image:
+            'https://www.ft.com/__origami/service/image/v2/images/raw/https%3A%2F%2Fs3-ap-northeast-1.amazonaws.com%2Fpsh-ex-ftnikkei-3937bb4%2Fimages%2F0%2F6%2F0%2F9%2F15379060-1-eng-GB%2FR20180823%20Maggi%20noodle_2048x1152.jpg?source=nar-cms',
+        },
+        {
+          name: 'maggie',
+          price: '20',
+          image:
+            'https://www.ft.com/__origami/service/image/v2/images/raw/https%3A%2F%2Fs3-ap-northeast-1.amazonaws.com%2Fpsh-ex-ftnikkei-3937bb4%2Fimages%2F0%2F6%2F0%2F9%2F15379060-1-eng-GB%2FR20180823%20Maggi%20noodle_2048x1152.jpg?source=nar-cms',
+        },
+        {
+          name: 'Classmate',
+          price: '100',
+          image:
+            'https://images-na.ssl-images-amazon.com/images/I/71adtAB2FZL.jpg',
+        },
+        {
+          name: 'Classmate',
+          price: '100',
+          image:
+            'https://images-na.ssl-images-amazon.com/images/I/71adtAB2FZL.jpg',
+        },
+        {
+          name: 'Classmate',
+          price: '100',
+          image:
+            'https://images-na.ssl-images-amazon.com/images/I/71adtAB2FZL.jpg',
+        },
+        {
+          name: 'Classmate',
+          price: '100',
+          image:
+            'https://images-na.ssl-images-amazon.com/images/I/71adtAB2FZL.jpg',
+        },
+        {
+          name: 'maggie',
+          price: '20',
+          image:
+            'https://www.ft.com/__origami/service/image/v2/images/raw/https%3A%2F%2Fs3-ap-northeast-1.amazonaws.com%2Fpsh-ex-ftnikkei-3937bb4%2Fimages%2F0%2F6%2F0%2F9%2F15379060-1-eng-GB%2FR20180823%20Maggi%20noodle_2048x1152.jpg?source=nar-cms',
+        },
+        {
+          name: 'maggie',
+          price: '20',
+          image:
+            'https://www.ft.com/__origami/service/image/v2/images/raw/https%3A%2F%2Fs3-ap-northeast-1.amazonaws.com%2Fpsh-ex-ftnikkei-3937bb4%2Fimages%2F0%2F6%2F0%2F9%2F15379060-1-eng-GB%2FR20180823%20Maggi%20noodle_2048x1152.jpg?source=nar-cms',
+        },
+        {
+          name: 'maggie',
+          price: '20',
+          image:
+            'https://www.ft.com/__origami/service/image/v2/images/raw/https%3A%2F%2Fs3-ap-northeast-1.amazonaws.com%2Fpsh-ex-ftnikkei-3937bb4%2Fimages%2F0%2F6%2F0%2F9%2F15379060-1-eng-GB%2FR20180823%20Maggi%20noodle_2048x1152.jpg?source=nar-cms',
+        },
+        {
+          name: 'maggie',
+          price: '20',
+          image:
+            'https://www.ft.com/__origami/service/image/v2/images/raw/https%3A%2F%2Fs3-ap-northeast-1.amazonaws.com%2Fpsh-ex-ftnikkei-3937bb4%2Fimages%2F0%2F6%2F0%2F9%2F15379060-1-eng-GB%2FR20180823%20Maggi%20noodle_2048x1152.jpg?source=nar-cms',
+        },
+        {
+          name: 'Classmate',
+          price: '20',
+          image:
+            'https://images-na.ssl-images-amazon.com/images/I/71adtAB2FZL.jpg',
+        },
+        {
+          name: 'Classmate',
+          price: '20',
+          image:
+            'https://images-na.ssl-images-amazon.com/images/I/71adtAB2FZL.jpg',
+        },
+        {
+          name: 'Classmate',
+          price: '20',
+          image:
+            'https://images-na.ssl-images-amazon.com/images/I/71adtAB2FZL.jpg',
+        },
+        {
+          name: 'Classmate',
+          price: '20',
+          image:
+            'https://images-na.ssl-images-amazon.com/images/I/71adtAB2FZL.jpg',
+        },
+      ],
     });
   }, []);
-  console.log(data);
   return data ? (
     <div>
       <Header
@@ -50,6 +170,49 @@ const ShopComp = (props) => {
         </Typography>
       </Parallax>
       <TopProds />
+
+      <Paper>
+        <Container>
+          <Grid container>
+            {data.products.slice(page * 9 - 9, page * 9).map((prod, i) => {
+              return (
+                <Grid item xs={4}>
+                  <Card className={classes.root}>
+                    <CardActionArea>
+                      <CardMedia
+                        className={classes.media}
+                        image={prod.image}
+                        title={prod.name}
+                      />
+                      <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                          {prod.name}
+                        </Typography>
+                      </CardContent>
+                    </CardActionArea>
+                    <CardActions>
+                      <Typography>Price: {prod.price}</Typography>
+                      <Button size="small" color="primary">
+                        Learn More
+                      </Button>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              );
+            })}
+          </Grid>
+          <Grid container>
+            <Grid className={classes.paginationAdjust} item xs={12}>
+              <Pagination
+                onChange={handleChangePage}
+                page={page}
+                count={Math.round(data.products.length / 9)}
+                color="primary"
+              />
+            </Grid>
+          </Grid>
+        </Container>
+      </Paper>
       <div className={classes.main}>
         <Grid container>
           <Grid item sm={12}>
--- test.js
+++ test.js
@@ -1,7 +1,27 @@
+const { resolve } = require('path');
+
 const db = require('./models/firebaseSDK').db;
 
-db.collection('users').get().then((docs) => {
-  docs.forEach((doc) => {
-    console.log(doc.data())
-  })
-})
+// db.collection('users').get().then((docs) => {
+//   docs.forEach((doc) => {
+//     console.log(doc.data())
+//   })
+// })
+
+var a = () => {
+  return new Promise((resolve) => {
+    resolve(
+      [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 5].map((i, j) => {
+        return {
+          name: 'maggie',
+          price: '20',
+          image: '',
+        };
+      })
+    );
+  });
+};
+
+a().then((data) => {
+  console.log(data);
+});
