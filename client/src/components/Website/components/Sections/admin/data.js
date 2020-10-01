ce9154fa64ecc9b4d3a58adb306eceb8f04f7d6a
mendex <mendex@root.root12>
2020-10-01 13:17:56 +0530

    add search vala thing

--- client/src/components/Website/components/Sections/admin/data.js
+++ client/src/components/Website/components/Sections/admin/data.js
@@ -17,6 +17,22 @@ var data = [
     media: '',
     modifiedAt: '27/0302019',
   },
+  {
+    // id : uuid(),
+    title: 'Britania',
+    totalDownloads: '594',
+    description: 'A quality bisciut.',
+    media: '',
+    modifiedAt: '27/0302019',
+  },
+  {
+    // id : uuid(),
+    title: 'Maggi ',
+    totalAvailable: '612',
+    description: 'cheap Shit.',
+    media: '',
+    modifiedAt: '27/0302019',
+  },
 ];
 
 export default data;
